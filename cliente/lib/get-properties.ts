90cbe8d8d5889e60c3c0b857f5a8e03b0e5d7298
MarcoA366 <marco_95_5@yahoo.com>
2025-11-24 01:09:45 -0300

    md

--- cliente/lib/get-properties.ts
+++ cliente/lib/get-properties.ts
@@ -1,19 +1,58 @@
+// import { fetchFromStrapi, STRAPI_URL } from "./api";
+// import { Property } from "@/types/property";
+
+// export async function getProperties(): Promise<Property[]> {
+//   const data = await fetchFromStrapi("/api/properties?populate=images");
+// console.log("data properties: ", data)
+//   return data.data.map((p: any) => {
+//     const attributes = p.attributes || p;
+
+//     const imageUrls =
+//       attributes.images?.data?.map(
+//         (img: any) => `${STRAPI_URL}${img.attributes.url}`
+//       ) ||
+//       attributes.images?.map((img: any) => `${STRAPI_URL}${img.url}`) ||
+//       [];
+
+//     const descriptionText = Array.isArray(attributes.description)
+//       ? attributes.description
+//           .map((d: any) => d.children?.map((c: any) => c.text).join(" "))
+//           .join(" ")
+//       : attributes.description || "";
+
+//     return {
+//       id: p.id,
+//       slug: attributes.slug || `propiedad-${p.id}`,
+//       title: attributes.title,
+//       type: attributes.type,
+//       address: attributes.address,
+//       city: attributes.city,
+//       price: `$${attributes.price?.toLocaleString("es-AR")}`,
+//       description: descriptionText,
+//       image: imageUrls[0] || "",
+//       images: imageUrls,
+//     };
+//   });
+// }
+
+
+
 import { fetchFromStrapi, STRAPI_URL } from "./api";
 import { Property } from "@/types/property";
 
 export async function getProperties(): Promise<Property[]> {
   const data = await fetchFromStrapi("/api/properties?populate=images");
-console.log("data properties: ", data)
-  return data.data.map((p: any) => {
-    const attributes = p.attributes || p;
 
+  return data.data.map((p: any) => {
+    const attributes = p.attributes;
+
+    // imágenes → estructura REAL de Strapi
     const imageUrls =
       attributes.images?.data?.map(
         (img: any) => `${STRAPI_URL}${img.attributes.url}`
-      ) ||
-      attributes.images?.map((img: any) => `${STRAPI_URL}${img.url}`) ||
-      [];
+      ) || [];
 
+    // description (RichText)
     const descriptionText = Array.isArray(attributes.description)
       ? attributes.description
           .map((d: any) => d.children?.map((c: any) => c.text).join(" "))
