7edce1ef6876f2f065e7f3db2109207aaad98a27
MarcoA366 <marco_95_5@yahoo.com>
2025-12-01 15:00:14 -0300

    md

--- cliente/lib/api.ts
+++ cliente/lib/api.ts
@@ -1,12 +1,29 @@
 // export const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
 export const STRAPI_URL = "https://giving-pleasure-c5847554d0.strapiapp.com";
 
+// export async function fetchFromStrapi(path: string) {
+//   const url = `${STRAPI_URL}${path}`;
+
+//   const res = await fetch(url, {
+//     cache: "no-store",
+//     // next: { revalidate: 60 }, // si lo necesitás más adelante
+//   });
+
+//   if (!res.ok) {
+//     const text = await res.text();
+//     console.error(`❌ Error en Strapi: ${res.status} ${res.statusText}`);
+//     console.error("📄 Respuesta:", text);
+//     throw new Error(`Strapi devolvió ${res.status}`);
+//   }
+
+//   return res.json();
+// }
+
 export async function fetchFromStrapi(path: string) {
   const url = `${STRAPI_URL}${path}`;
 
   const res = await fetch(url, {
-    cache: "no-store",
-    // next: { revalidate: 60 }, // si lo necesitás más adelante
+    next: { revalidate: 60 }, // <-- cada 60 segundos se actualiza automáticamente
   });
 
   if (!res.ok) {
--- cliente/lib/get-home.ts
+++ cliente/lib/get-home.ts
@@ -1,27 +1,3 @@
-// import { fetchFromStrapi, STRAPI_URL } from "./api";
-
-// export async function getHome() {
-//   console.log("strapi_url", STRAPI_URL)
-//   try {
-//     const data = await fetchFromStrapi("/api/home?populate=image");
-// console.log("data: ", data)
-//     const home = data?.data || {};
-
-//     return {
-//       title: home.title || null,
-//       subtitle: home.subtitle || null,
-//       // imageUrl: home.image?.url ? `${STRAPI_URL}${home.image.url}` : null,
-//       imageUrl: home.image?.data?.attributes?.url
-//   ? `${STRAPI_URL}${home.image.data.attributes.url}`
-//   : null,
-
-//     };
-//   } catch (error) {
-//     console.error("💥 Error general en getHome():", error);
-//     return { title: null, subtitle: null, imageUrl: null };
-//   }
-// }
-
 import { fetchFromStrapi } from "./api";
 
 export async function getHome() {
